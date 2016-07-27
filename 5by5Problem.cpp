098eee6fedf8778d5e293649b89ca4852c0577ee
audaxbufetejr <audaxbufetejr@gmail.com>
2016-07-27 10:35:43 -0700

    Add files via upload

---
+++ 5by5Problem.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	cout << "AJ's Method for Assignment Problems" << endl << endl;
+	cout << "This will be used for only a 5 by 5 assignment problem." << endl;
+	cout << "Enter your 5 by 5 assignment matrix as your values appear from left to right, then top to bottom:" << endl;
+
+	int assignMatrix[5][5];
+
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			cin >> assignMatrix[i][j];
+		}
+	}
+
+	int a, b, c, d, e; //all these for loops and ifs can be written as a separate function
+
+	for (int i = 0; i < 5; i++)
+	{
+		if (i == 0)
+		{
+			a = assignMatrix[0][0];
+
+			for (int j = 0; j < 5; j++)
+			{
+				if (assignMatrix[i][j] < a)
+					a = assignMatrix[i][j];
+
+			}
+		}
+
+		if (i == 1)
+		{
+			b = assignMatrix[1][0];
+
+			for (int j = 0; j < 5; j++)
+			{
+				if (assignMatrix[i][j] < b)
+					b = assignMatrix[i][j];
+			}
+		}
+
+		if (i == 2)
+		{
+			c = assignMatrix[2][0];
+
+			for (int j = 0; j < 5; j++)
+			{
+				if (assignMatrix[i][j] < c)
+					c = assignMatrix[i][j];
+			}
+		}
+
+		if (i == 3)
+		{
+			d = assignMatrix[3][0];
+
+			for (int j = 0; j < 5; j++)
+			{
+				if (assignMatrix[i][j] < d)
+					d = assignMatrix[i][j];
+			}
+		}
+
+		if (i == 4)
+		{
+			e = assignMatrix[4][0];
+
+			for (int j = 0; j < 5; j++)
+			{
+				if (assignMatrix[i][j] < e)
+					e = assignMatrix[i][j];
+			}
+		}
+	}
+
+	//cout << endl << a << " " << b << " " << c << " " << d << " " << e << endl;
+	
+	system("pause");
+	return 0;
+}
